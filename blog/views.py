6fdab545d23495bd434c4b70dfb5baae2ff1c66e
AlexJ <aleksandarjovcevski@gmail.com>
2021-11-12 11:58:22 +0100

    added drafts and delete

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseForbidden
 from .models import Post
 from .forms import PostForm
 from django.utils import timezone
 
 def post_list(request):
-    posts = Post.objects.order_by('-published_date')
+    posts = Post.objects.exclude(published_date__isnull=True).order_by('-published_date')
     return render(request, 'blog/post_list.html', { 'posts' : posts })
 
 def post_detail(request, pk):
@@ -17,23 +18,41 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post_draft_list')
+            # return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
+
     if request.method == "POST":
+        if request.user.pk != post.author.pk:
+            return HttpResponseForbidden()
+
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post_list')
+            # return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).filter(author__pk=request.user.pk).order_by('-created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_list', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
